33c86d5327bdebe98656e87a080daafd62ca35e0
Loh Jing Yen <lohjingyen@gmail.com>
2017-10-11 15:26:36 +0800

    Sample table routing

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+"""Sample Flask server for IoT
+
+This is the entry point of the backend
+
+"""
+
 from flask import Flask
-from flask import url_for
+from flask import render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def hello_world():
     return 'Hello, World!'
+
+@app.route('/table/<int:table_id>')
+def show_menu(table_id=None):
+    return render_template('sample.html', table_id=table_id)
