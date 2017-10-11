dfde6a3db02032d156e36b88e5bd25ed959f1368
Loh Jing Yen <lohjingyen@gmail.com>
2017-10-11 15:32:27 +0800

    Change default route to return page

--- main.py
+++ main.py
@@ -9,13 +9,18 @@ from flask import render_template
 app = Flask(__name__)
 
 
+"""App default route
+"""
 @app.route('/')
-def hello_world():
-    return 'Hello, World!'
+def landing_page():
+    return render_template('index.html')
 
 """Per table routing
 So Josh, this will take in an argument called table_id
 and route to a page (sample.html in this case)
+
+sample.html is supposed to be a jinja template (like razor)
+and process something based on table number
 """
 @app.route('/table/<int:table_id>')
 def show_menu(table_id=None):
